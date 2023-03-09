11c40343b36ed70269af4dcc6c368eac1834fe03
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-03-09 19:29:57 +0100

    commit

--- python/Evaluation.py
+++ python/Evaluation.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as elementtree
 
 import openai
 
+openai.api_key = "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"
 dict_entity = dict()
 dict_semantic = dict()
 debug = False
@@ -38,7 +39,9 @@ def extract_values_from_file(filepath, path="../documents/xmi/"):
         end = int(child.attrib["end"])
         text = file_text[begin:end]
         dict_entity[file_text][id] = text
-        print(f"text={file_text} id={id} result={dict_entity[file_text][id]}" if debug else "", end="\r")
+        global debug
+        if debug:
+            print(f"text={file_text} id={id} result={dict_entity[file_text][id]}")
     for child in root:
         if "SemanticRelations" not in child.tag:
             continue
@@ -48,17 +51,37 @@ def extract_values_from_file(filepath, path="../documents/xmi/"):
         id = child.attrib["{http://www.omg.org/XMI}id"]
         # governor relation dependent
         dict_semantic[file_text][id] = (governor, relation, dependent)
-        print(f"text={file_text} id={id} result={dict_semantic[file_text][id]}" if debug else "", end="\r")
+        if debug:
+            print(f"text={file_text} id={id} result={dict_semantic[file_text][id]}")
+
+        print(governor, relation, dependent)
 
 
-def evaluate(gold_answers, ai_answers):
-    pass
+def format_converter(semantic_dict):
+    for text, value in semantic_dict.items():
+        results = []
+        for id, triple in value.items():
+            # old format: governor relation dependent new format: <triplet> governor <sub> dependent <obj> relation
+            print(f"old: {triple[0]} {triple[1]} {triple[2]}")
+            print(f"<triplet> {triple[0]} <sub> {triple[2]} <obj> {triple[1]}")
+            result = f"<triplet> {triple[0]} <sub> {triple[2]} <obj> {triple[1]}"
+            results.append(result)
+        # reperate all results in result in one string seperated by "  "
+        final_string = "  ".join(results)
+        file_saver(final_string)
+
+
+def file_saver(text):
+    with open("../documents/results/result.csv", "a", encoding="UTF-8") as file:
+        file.write(text + "\n")
 
 
 def main():
     files = [filename for filename in os.listdir("../documents/xmi") if filename.endswith(".xmi")]
     for filename in files:
         extract_values_from_file(filename)
+    file_saver("triplets")
+    format_converter(dict_semantic)
 
 
 if __name__ == "__main__":
