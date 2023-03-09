8f2dc3b58f50735d39e8608addf72045c206706a
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-03-09 16:11:10 +0100

    commit

--- python/Evaluation.py
+++ python/Evaluation.py
@@ -3,6 +3,10 @@ import xml.etree.ElementTree as elementtree
 
 import openai
 
+dict_entity = dict()
+dict_semantic = dict()
+debug = False
+
 
 def generate_response(input_text, prefix=""):
     completion = openai.ChatCompletion.create(
@@ -13,13 +17,9 @@ def generate_response(input_text, prefix=""):
     return completion.choices[0]["text"]
 
 
-dict_entity = dict()
-dict_semantic = dict()
-
-
 def extract_values_from_file(filepath, path="../documents/xmi/"):
     """
-    dict_ent -> "text" -> dict -> "id" -> (param, value)
+    dict_ent -> "text" -> dict -> "id" ->  value
     dict_sem -> "text" -> dict -> "id" -> (object, relation, value)
     """
     tree = elementtree.parse(path + filepath)
@@ -31,20 +31,35 @@ def extract_values_from_file(filepath, path="../documents/xmi/"):
     for child in root:
         if "NamedEntity" not in child.tag:
             continue
-        if "value" not in child.attrib or child.attrib["value"] == "REGEX":
-            continue
+        # if "value" in child.attrib and child.attrib["value"] == "REGEX":
+        #     continue TODO: should be contained?
         id = child.attrib["{http://www.omg.org/XMI}id"]
         begin = int(child.attrib["begin"])
         end = int(child.attrib["end"])
         text = file_text[begin:end]
-        dict_entity[file_text][id] = (child.attrib["value"], text)
-        print(f"text={file_text} id={id} result={dict_entity[file_text][id]}")
+        dict_entity[file_text][id] = text
+        print(f"text={file_text} id={id} result={dict_entity[file_text][id]}" if debug else "", end="\r")
+    for child in root:
+        if "SemanticRelations" not in child.tag:
+            continue
+        dependent = dict_entity[file_text][child.attrib["Dependent"]]
+        governor = dict_entity[file_text][child.attrib["Governor"]]
+        relation = child.attrib["Relation"]
+        id = child.attrib["{http://www.omg.org/XMI}id"]
+        # governor relation dependent
+        dict_semantic[file_text][id] = (governor, relation, dependent)
+        print(f"text={file_text} id={id} result={dict_semantic[file_text][id]}" if debug else "", end="\r")
 
 
 def evaluate(gold_answers, ai_answers):
     pass
 
 
-files = [filename for filename in os.listdir("../documents/xmi") if filename.endswith(".xmi")]
-for filename in files:
-    extract_values_from_file(filename)
+def main():
+    files = [filename for filename in os.listdir("../documents/xmi") if filename.endswith(".xmi")]
+    for filename in files:
+        extract_values_from_file(filename)
+
+
+if __name__ == "__main__":
+    main()
